b78cd39c12e1f904cd6321b202cf0b6f7d144211
baloxq <balakinaolga2006@gmail.com>
2026-04-15 15:14:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,19 @@ function logout() {
     window.location.href = "index.html";
 }
 
-function showPage(pageId) {
-    document.querySelectorAll(".page").forEach(p => p.classList.remove("active-page"));
+function showPage(pageId, element) {
+
+    // переключение страниц
+    document.querySelectorAll(".page").forEach(p => {
+        p.classList.remove("active-page");
+    });
+
     document.getElementById(pageId).classList.add("active-page");
 
-    document.querySelectorAll("aside li").forEach(li => li.classList.remove("active"));
-    event.target.classList.add("active");
+    // активная кнопка в sidebar
+    document.querySelectorAll("aside li").forEach(li => {
+        li.classList.remove("active");
+    });
+
+    element.classList.add("active");
 }
