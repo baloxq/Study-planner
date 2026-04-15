84e6b63745de547ea2223b60e50105cf9bfde726
baloxq <balakinaolga2006@gmail.com>
2026-04-15 17:33:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function login(){window.location.href='dashboard.html';}
+function register(){alert('Registered!');window.location.href='index.html';}
+function logout(){window.location.href='index.html';}
