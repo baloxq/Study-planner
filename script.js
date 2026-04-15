f38d07a72ac473e6f66d70026116b2f0f31682a5
baloxq <balakinaolga2006@gmail.com>
2026-04-15 18:03:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function login(){window.location.href='dashboard.html';}
-function register(){alert('Registered!');window.location.href='index.html';}
-function logout(){window.location.href='index.html';}
